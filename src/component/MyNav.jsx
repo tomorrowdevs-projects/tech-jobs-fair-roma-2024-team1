5afff72b99800ec1dd8942f3feed3d4629747be0
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-16 22:02:46 +0400

    added a logo and a name

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,29 +1,35 @@
 import { SignOutButton, useUser } from "@clerk/clerk-react";
 import { Container, Dropdown, Navbar } from "react-bootstrap";
 import styles from "../component/MyNav.module.css";
+import logo from "../assets/logo.svg";
+
 const MyNav = () => {
   const { isSignedIn, user } = useUser();
   return (
     <>
-      <Navbar className={`${styles.navBar} py-0 `}>
+      <Navbar className={`${styles.navBar} py-0`}>
         <Container>
+          <Navbar.Brand className={`${styles.brand}`}>
+            <img src={logo} alt="RitmoGiornaliero Logo" className={`${styles.logo}`} />
+            <span className={styles.appName}>RitmoGiornaliero</span>
+          </Navbar.Brand>
+
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
               <Dropdown autoClose="outside">
                 <Dropdown.Toggle variant="btn" id="dropdown-autoclose-outside" className={`${styles.noCaret} ${styles.userToggle}`}>
-                {isSignedIn && (
-                  <div className={styles.userInfo}>
-                    <img src={user.imageUrl} alt="" className={`${styles.imgProfile} rounded-circle`} />
-                    <div className={`${styles.navText} mt-3`}>Hello, {user.firstName}</div>
-                  </div>
-                )}
+                  {isSignedIn && (
+                    <div className={styles.userInfo}>
+                      <img src={user.imageUrl} alt="" className={`${styles.imgProfile} mt-3 rounded-circle`} />
+                      <div className={`${styles.navText} mt-3`}>Hello, {user.firstName}</div>
+                    </div>
+                  )}
                 </Dropdown.Toggle>
                 <Dropdown.Menu className={`${styles.navBar} ${styles.narrowDropdown}`}>
                   <Dropdown.Item className={`${styles.navBar}`}>
-                    {" "}
                     <SignOutButton>
-                      <span className={`${styles.navText} py-3 m-0 `}>Sign out</span>
+                      <span className={`${styles.navText} py-3 m-0`}>Sign out</span>
                     </SignOutButton>
                   </Dropdown.Item>
                 </Dropdown.Menu>
@@ -35,4 +41,5 @@ const MyNav = () => {
     </>
   );
 };
+
 export default MyNav;
