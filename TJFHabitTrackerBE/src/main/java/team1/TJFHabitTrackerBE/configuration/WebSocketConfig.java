1004a9f8509803cff84783c27362e9361abe51d4
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-10-09 18:46:04 +0200

    update

--- TJFHabitTrackerBE/src/main/java/team1/TJFHabitTrackerBE/configuration/WebSocketConfig.java
+++ TJFHabitTrackerBE/src/main/java/team1/TJFHabitTrackerBE/configuration/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-notifications")
-                .setAllowedOrigins(String.valueOf(Arrays.asList("http://localhost:5173/", "https://ritmogiornaliero.netlify.app/")))
+                .setAllowedOrigins("http://localhost:5173/", "https://ritmogiornaliero.netlify.app/")
                 .withSockJS();
     }
 }
